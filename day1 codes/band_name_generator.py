366cd3661cd33a2bd3a423664a8d012e5db727c0
Ubbix1 <88034859+Ubbix1@users.noreply.github.com>
2023-03-04 23:04:51 +0530

    Add files via upload

---
+++ day1 codes/band_name_generator.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print("Band Name Generator:")
+print("....................")
+#2. Ask the user for the place short form .
+place = input("What is your place short form?\n")
+#3. Ask the user for their nicknames to findout band name.
+nick_name = input("what is your nickname? \n")
+#4. Combine the name of their place and nick name and show them their band name.
+print("your band name is " + place  + " " + nick_name)
+
+
